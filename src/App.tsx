f59fd5b0173d8458d1518f3a52d3deca8c385074
unknown <mejaras@gmail.com>
2025-08-25 13:41:44 +0300

    added button to clear all the storage for convenience

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Button } from "./components/ui/button";
 import { EuroBasket } from "./Tables/EuroBasket/EuroBasket";
 import { Premier } from "./Tables/Premier/Premier";
 import { Wimbledon } from "./Tables/Wimbledon/Wimbledon";
 
 export const App = () => {
+  const handleClearAll = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <div className="flex  flex-wrap gap-4 justify-center items-center h-screen p-4 overflow-x-auto">
+      <Button
+        onClick={handleClearAll}
+        variant="destructive"
+        className="absolute top-4 left-4"
+      >
+        Clear All
+      </Button>
       <Premier />
       <EuroBasket />
       <Wimbledon />
